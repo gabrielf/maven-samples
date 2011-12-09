a5a75698b2fe02345bb0aef158709def5b716ca3
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2011-12-09 02:54:02 +0100

    Changed indent to 2 spaces.

--- multi-module/server/src/main/java/com/example/Greeter.java
+++ multi-module/server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,15 @@ package com.example;
  */
 public class Greeter {
 
-    /**
-     * This is a constructor.
-     */
-    public Greeter() {
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
 
-    }
+  }
 
-    //TODO: Add javadoc comment
-    public String greet(String someone) {
-        return String.format("Hello, %s!", someone);
-    }
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
--- single-module/src/test/java/com/example/TestGreeter.java
+++ single-module/src/test/java/com/example/TestGreeter.java
@@ -2,31 +2,32 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.matchers.JUnitMatchers.containsString;
-import static org.junit.Assert.assertThat;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class TestGreeter {
 
-    private Greeter greeter;
+  private Greeter greeter;
 
-    @Before
-    public void setup() {
-        greeter = new Greeter();
-    }
+  @Before
+  public void setup() {
+    greeter = new Greeter();
+  }
 
-    @Test
-    public void greetShouldIncludeTheOneBeingGreeted() {
-        String someone = "World";
+  @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
 
-        assertThat(greeter.greet(someone), containsString(someone));
-    }
+    assertThat(greeter.greet(someone), containsString(someone));
+  }
 
-    @Test
-    public void greetShouldIncludeGreetingPhrase() {
-        String someone = "World";
+  @Test
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World";
 
-        assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-    }
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
